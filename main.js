38d9348368ce4363e7e080a0abd1d6834e6c911f
miswhiramon <yh7500u@gmail.com>
2021-09-04 22:07:21 +0900

    c

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function setup() {
 
     textSize(32);
     place_disk=loadSound('./オセロ・コマ01.mp3');
-    normal_state=loadSound('./アトリエと電脳世界.mp3');
+    normal_state=loadSound('./アトリエと電脳世界.aac');
     menhera=loadSound('./不穏.mp3');
 }
 
